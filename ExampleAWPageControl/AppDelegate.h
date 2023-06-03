e1d4ece71144fba889eb524e37fe58ba8de246bd
Emck <emck.do@gmail.com>
2023-06-03 10:55:28 +0800

    create a new project by macOS

---
+++ ExampleAWPageControl/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ExampleAWPageControl
+//
+//  Created by Emck on 2023/6/3.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
